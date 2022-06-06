4d6d85072ebee1a15b047b31ce564cf081db7001
steimz <vladyslav.kolomiets@protonmail.com>
2022-06-06 19:04:47 +0300

    Query restriction: origin and destination cannot be the same

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,7 +8,11 @@ class FlightsController < ApplicationController
     @date_options = @flights.map { |f| [f.start_time.strftime('%d %^b %Y'), f.start_time] }.uniq
     @available_flights = Flight.where(departure_airport_id: params[:departure_airport_id],
                                       arrival_airport_id: params[:arrival_airport_id],
-                                      start_time: params[:start_time])
+                                      start_time: params[:start_time]).where('departure_airport_id != arrival_airport_id')
+    if params[:departure_airport_id] == params[:arrival_airport_id] && params.has_value?('Search')
+      flash.now[:alert] = 'Please choose two different origin and destination locations!'
+      render :index
+    end
   end
 
   # GET /flights/1 or /flights/1.json
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,11 @@ airports = []
   airports << Airport.create({ code: Faker::Address.country_code_long })
 end
 
-
+date1 = "#{Time.now.day}-#{Time.now.month}-#{Time.now.year}"
+date2 = "#{Time.now.day + 5}-#{Time.now.month}-#{Time.now.year}"
 500.times do
-   Flight.create(departure_airport: airports.sample, arrival_airport: airports.sample,
-    start_time: Faker::Date.between(from: '01-01-2022', to: '05-01-2022') , duration: "#{rand(2..12)}:00", price: rand(500..900))
+  Flight.create(departure_airport: airports.sample, arrival_airport: airports.sample,
+                start_time: Faker::Date.between(from: date1, to: date2), duration: "#{rand(2..12)}:00", price: rand(500..900))
 end
-  
 
 p "Created #{Flight.count} flights"
